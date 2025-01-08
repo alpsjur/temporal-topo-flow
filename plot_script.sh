72d3633b7589152e3fe183cb2d27d6b4c0d4123c
Anna Lina Sjur <alpsjur@runbox.com>
2025-01-08 14:53:36 +0100

    updated config path

--- plot_script.sh
+++ plot_script.sh
@@ -4,7 +4,7 @@ FILENAMES=("slope/code/plot_Hcontour_analysis.py" "slope/code/plot_streamline_ev
 
 for FILENAME in "${FILENAMES[@]}"
 do
-    for CONFIG in slope/configs/*
+    for CONFIG in slope/configs/run/*
     do
         python "$FILENAME" "$CONFIG"
     done
