597f12b9c61885786e54b562347277e2ae0ea0d8
Anna Lina Sjur <alpsjur@runbox.com>
2026-02-05 10:40:04 +0100

    fix bash dockstring

--- scripts/run_animate_fileds.sh
+++ scripts/run_animate_fileds.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-"""
-Script to animate model output for multiple configurations.
-Run from command line:
-    bash scripts/run_animate_fileds.sh
-"""
+
+# Script to animate model output for multiple configurations.
+# Run from command line:
+#     bash scripts/run_animate_fileds.sh
+
 
 # spesify which configs to animate
 CONFIGS=("configs/baseline_forcing/short.json" "configs/baseline_forcing/long.json")
